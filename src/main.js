ed9528ccffad1c08ec0204932a5af80520456893
bishaolong <840321062@qq.com>
2019-08-18 14:30:10 +0800

    f7

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Framework7 from 'framework7/framework7.esm.bundle.js';
+
+import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js'
+// Import F7 Styles
+import 'framework7/css/framework7.css';
+
+// Import Icons and App Custom Styles
+import './css/icons.css';
+import './css/app.css';
+
+Framework7.use(Framework7Vue)
+
+
 Vue.config.productionTip = false
 
 new Vue({
